6d7d260d784f511f28ebbca1facfd712e7fe3f13
Dude-source-cpu <jakepurchases@gmail.com>
2025-11-23 19:42:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 export const clientId = 'cecffa573fbf4257ad5a1a3bd35e6bf2';
 // To access from another device, replace '127.0.0.1' with your computer's LAN IP (e.g., 192.168.1.X)
 // AND add that same URL to your Spotify Dashboard Redirect URIs.
-export const redirectUri = 'http://127.0.0.1:8888/index.html';
+export const redirectUri = 'https://dude-source-cpu.github.io./Spotify-Custom-Player-1-2-1-1-1-1-1-1-1-1-1-1-11/index.html';
 export const scopes = [
     'user-read-currently-playing',
     'user-modify-playback-state',
